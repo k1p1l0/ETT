99db6afa44cd8d0af5eef7312357790ea3d16905
k1p1l0 <s0ht@mail.ru>
2016-08-17 00:36:42 +0300

    bower_components for gh-page

---
+++ bower_components/sshelp.js/sshelp.js
@@ -0,0 +1,39 @@
+( function (window) {
+
+	function Constructor () {
+		var module = {};
+		
+		module.log = console.log.bind(console),
+		module.dir = console.dir.bind(console);
+
+		module.valid = function (string) {
+			if (string <= 0) {
+				string = 'No information';
+			}
+
+			return string;
+		};
+
+		module.extend = function (Parent, Child) {
+			var Surrogat = function(){};
+			Surrogat.prototype = Parent.prototype;
+
+			Child.prototype = new Surrogat();
+			Child.prototype.constuctor = Child;
+			Child.prototype.super = Parent.prototype;
+		};
+
+		module.getTypeString = function (obj) {
+		  	return Object.prototype.toString.call(obj).slice(8, -1);
+		};
+
+		module.createId = function (length) {
+			return Math.random().toString(36).substring(length);
+		};
+
+		return module;
+	}
+
+	window.SS = Constructor();
+
+} )(typeof window !== "undefined" ? window : this);
